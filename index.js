ec77b52b77c51d9a7ba3ced773ff81cb4368c880
John Hornick <jhornick@live.com>
2023-02-02 20:57:01 -0500

    Added update method to table class, update option to inquirer interface.

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const db = mysql.createConnection(
 
 const RequireAnswer = ans => ans.trim() !== '';
 
+//Creates the tables in the database if they do not already exist
 function CreateTables() {
     const departments = new Table('departments');
     const roles = new Table('roles','departments','department_id',true);
@@ -41,6 +42,7 @@ function CreateTables() {
     employees.create(db,false);
 }
 
+//Welcome message when app starts
 function DisplayTitleCard() {
     console.log('\n\n' +
         '--------------------------------\n' + 
@@ -48,6 +50,7 @@ function DisplayTitleCard() {
         '--------------------------------\n');
 }
 
+//Display menu with options for user to pick which functionality to use
 function MainMenu() {
     inquirer.prompt([
         { name:"choice", message:"What would you like to do?", type:'list', choices:[
@@ -64,29 +67,38 @@ function MainMenu() {
     .then(async answers => {
         switch(answers.choice) {
             case 0:
+                //view employee table
                 await ViewTable(employees);
                 MainMenu();
                 break;
             case 1:
+                //send user to add an employee
                 AddData(employees);
                 break;
             case 2:
+                //send user to update role of an employee
+
                 break;
             case 3:
+                //view role table
                 await ViewTable(roles);
                 MainMenu();
                 break;
             case 4:
+                //send user to add a role
                 AddData(roles);
                 break;
             case 5:
+                //view department table
                 await ViewTable(departments);
                 MainMenu();
                 break;
             case 6:
+                //send user to add a department
                 AddData(departments);
                 break;
             case 7:
+                //say bye and exit
                 console.log('Goodbye!');
                 process.exit();
             default:
@@ -100,44 +112,145 @@ function MainMenu() {
     });
 }
 
+//Go through inquirer process with user to determine data for new entry based on which table they're entering
+//Add the data and return to main menu
 async function AddData(table) {
-    //departments.addData([{field:'name',value:'"anothertestval"'}],db);
+    //populate inquirer questions based on which table they're entering data
     let questions = [];
     switch(table.name) {
         case 'departments':
             questions.push({ name:'name', message:'Enter name of department:', validate:RequireAnswer });
             break;
         case 'roles':
-            const depts = await departments.getData();
             questions.push({ name:'title', message:'Enter title of role:', validate:RequireAnswer });
             questions.push({ name:'salary', message:'Enter salary for role:', validate:RequireAnswer });
-            const dptoptions = [];
-            depts.forEach(dept => {
-                dptoptions.push({
-                    name:dept.name,
-                    value:dept.id
-                });
-            });
-            questions.push({ name:'department_id', message:'Role is in which department?', type:'list', choices:dptoptions });
+            questions.push({ name:'department_id', message:'Role is in which department?', type:'list', choices:await GetInquirerOptionsFromTable(departments,'name') });
             break;
         case 'employees':
-            const rls = await departments.getData();
             questions.push({ name:'first_name', message:'First name of employee:', validate:RequireAnswer });
             questions.push({ name:'last_name', message:'Last name of employee:', validate:RequireAnswer });
-            const rloptions = [];
-            rls.forEach(rl => {
-                rloptions.push({
-                    name:rl.name,
-                    value:rl.id
-                });
-            });
-            questions.push({ name:'role_id', message:'Employee is in which role?', type:'list', choices:rloptions });
+            questions.push({ name:'role_id', message:'Employee is in which role?', type:'list', choices:await GetInquirerOptionsFromTable(roles,'title') });
+            //set aside user choices for employee manager, then prepend "None" option which will return null
+            let managerOptions = await GetInquirerOptionsFromTable(employees,['first_name','last_name']);
+            managerOptions = [{name:'None',value:null}].concat(managerOptions);
+            questions.push({ name:'manager_id', message:'Who is employee\'s manager?', type:'list', choices:managerOptions });
             break;
         default:
             throw new Error('Invalid Table Passed to AddData()');
     }
+
+    //prompt user with above questions 
+    inquirer.prompt([
+        ...questions
+    ])
+    .then(async answers => {
+        //add data based on which table we're entering
+        switch(table.name) {
+            case 'departments':
+                await table.addData([
+                    {field:'name',value:Stringify(answers.name)}
+                ],db);
+                break;
+            case 'roles':
+                await table.addData([
+                    {field:'title',value:Stringify(answers.title)},
+                    {field:'salary',value:answers.salary},
+                    {field:'department_id',value:answers.department_id}
+                ],db);
+                break;
+            case 'employees':
+                dataToAdd = [
+                    {field:'first_name',value:Stringify(answers.first_name)},
+                    {field:'last_name',value:Stringify(answers.last_name)},
+                    {field:'role_id',value:answers.role_id}
+                ];
+                //don't add anything for manager_id if it was left null
+                if(answers.manager_id) {
+                    dataToAdd.push({field:'manager_id',value:answers.manager_id});
+                }
+                await table.addData(dataToAdd,db);
+                break;
+            default:
+                throw new Error('Invalid Table Passed to AddData()');
+        }
+        MainMenu();
+    })
+    .catch((err) => {
+        if (err) {
+            throw err;
+        }
+    });
 }
 
+//input validation to prevent an employee from managing itself
+const RequireDifferentEmployee = (inp,ans) => ans.employee !== inp;
+//Go through inquirer process with user to determine what data is changing to what
+//Update the data and return to main menu
+async function UpdateData(table,reftable) {
+    let questions = [];
+    if(table.name === 'employees' && reftable.name === 'employees') {
+        //we're updating employee manager
+        questions.push({ name:'employee', message:'Which employee is being updated?', type:'list', choices:await GetInquirerOptionsFromTable(table,['first_name','last_name'])});
+        questions.push({ name:'manager', message:'Who is the new manager?', type:'list', choices:await GetInquirerOptionsFromTable(reftable,['first_name','last_name']), validate:RequireDifferentEmployee });
+    } else if(table.name === 'employees' && reftable.name === 'roles') {
+        //we're updating employee role
+        questions.push({ name:'employee', message:'Which employee is being updated?', type:'list', choices:await GetInquirerOptionsFromTable(table,['first_name','last_name'])});
+        questions.push({ name:'role', message:'What is the new role?', type:'list', choices:await GetInquirerOptionsFromTable(reftable,'title') });
+    } else if(table.name === 'roles' && reftable.name === 'departments') {
+        //we're updating role's department
+        questions.push({ name:'role', message:'Which role is being updated?', type:'list', choices:await GetInquirerOptionsFromTable(table,'title')});
+        questions.push({ name:'department', message:'Role is moving to which department?', type:'list', choices:await GetInquirerOptionsFromTable(reftable,'name') });
+    } else {
+        console.log('Invalid Update Input');
+        return;
+    }
+
+    //prompt user with above questions
+    inquirer.prompt([
+        ...questions
+    ])
+    .then(async answers => {
+        //call on table to update data in correct cell based on input
+        if(table.name === 'employees' && reftable.name === 'employees') {
+            table.updateData(answers.employee,'manager_id',answers.manager);
+        } else if(table.name === 'employees' && reftable.name === 'roles') {
+            table.updateData(answers.employee,'role_id',answers.role);
+        } else if(table.name === 'roles' && reftable.name === 'departments') {
+            table.updateData(answers.role,'department_id',answers.department);
+        }
+    })
+    .catch((err) => {
+        if (err) {
+            throw err;
+        }
+    });
+}
+
+//format a string with ' around it so that it can be used as a string elsewhere (such as DB queries)
+function Stringify(inp) {
+    return "'" + inp + "'";
+}
+
+//gets an array of inquirer-formatted choices
+//displayed name will be the value in the table corresponding to the key given as identifier
+//returned value will be the 'id' property of the table 
+async function GetInquirerOptionsFromTable(table,identifier) {
+    const dat = await table.getData(db);
+    const options = [];
+    if(typeof identifier === 'string') {
+        identifier = [identifier];
+    }
+    dat.forEach(row => {
+        let optionname = identifier.map(str => row[str]).join(' ');
+        options.push({
+            name:optionname,
+            value:row.id
+        });
+    });
+    return options;
+}
+
+//displays table formatted in console
 async function ViewTable(table) {
     const dat = await table.getData(db);
     if(dat.length) {
