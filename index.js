74c252bc5a0a530e907f580620b588dd8b404d03
John Hornick <jhornick@live.com>
2023-02-02 21:30:08 -0500

    Data updating debugged, data viewing with filters added.

--- index.js
+++ index.js
@@ -57,11 +57,12 @@ function MainMenu() {
             {name:'View All Employees', value:0},
             {name:'Add Employee', value:1},
             {name:'Update Employee Role', value:2},
-            {name:'View All Roles', value:3},
-            {name:'Add Role', value:4},
-            {name:'View All Departments',value:5},
-            {name:'Add Department',value:6},
-            {name:'Quit', value:7}
+            {name:'Update Employee Manager', value:3},
+            {name:'View All Roles', value:4},
+            {name:'Add Role', value:5},
+            {name:'View All Departments',value:6},
+            {name:'Add Department',value:7},
+            {name:'Quit', value:8}
         ] }
     ])
     .then(async answers => {
@@ -77,27 +78,31 @@ function MainMenu() {
                 break;
             case 2:
                 //send user to update role of an employee
-
+                UpdateData(employees,roles);
                 break;
             case 3:
+                //send user to update manager of an employee
+                UpdateData(employees,employees);
+                break;
+            case 4:
                 //view role table
                 await ViewTable(roles);
                 MainMenu();
                 break;
-            case 4:
+            case 5:
                 //send user to add a role
                 AddData(roles);
                 break;
-            case 5:
+            case 6:
                 //view department table
                 await ViewTable(departments);
                 MainMenu();
                 break;
-            case 6:
+            case 7:
                 //send user to add a department
                 AddData(departments);
                 break;
-            case 7:
+            case 8:
                 //say bye and exit
                 console.log('Goodbye!');
                 process.exit();
@@ -182,8 +187,6 @@ async function AddData(table) {
     });
 }
 
-//input validation to prevent an employee from managing itself
-const RequireDifferentEmployee = (inp,ans) => ans.employee !== inp;
 //Go through inquirer process with user to determine what data is changing to what
 //Update the data and return to main menu
 async function UpdateData(table,reftable) {
@@ -191,7 +194,9 @@ async function UpdateData(table,reftable) {
     if(table.name === 'employees' && reftable.name === 'employees') {
         //we're updating employee manager
         questions.push({ name:'employee', message:'Which employee is being updated?', type:'list', choices:await GetInquirerOptionsFromTable(table,['first_name','last_name'])});
-        questions.push({ name:'manager', message:'Who is the new manager?', type:'list', choices:await GetInquirerOptionsFromTable(reftable,['first_name','last_name']), validate:RequireDifferentEmployee });
+        let managerOptions = await GetInquirerOptionsFromTable(employees,['first_name','last_name']);
+        managerOptions = [{name:'None',value:null}].concat(managerOptions);
+        questions.push({ name:'manager', message:'Who is the new manager?', type:'list', choices:managerOptions });
     } else if(table.name === 'employees' && reftable.name === 'roles') {
         //we're updating employee role
         questions.push({ name:'employee', message:'Which employee is being updated?', type:'list', choices:await GetInquirerOptionsFromTable(table,['first_name','last_name'])});
@@ -212,18 +217,48 @@ async function UpdateData(table,reftable) {
     .then(async answers => {
         //call on table to update data in correct cell based on input
         if(table.name === 'employees' && reftable.name === 'employees') {
-            table.updateData(answers.employee,'manager_id',answers.manager);
+            await table.updateData(answers.employee,'manager_id',answers.manager,db);
         } else if(table.name === 'employees' && reftable.name === 'roles') {
-            table.updateData(answers.employee,'role_id',answers.role);
+            await table.updateData(answers.employee,'role_id',answers.role,db);
         } else if(table.name === 'roles' && reftable.name === 'departments') {
-            table.updateData(answers.role,'department_id',answers.department);
+            await table.updateData(answers.role,'department_id',answers.department,db);
         }
+        MainMenu();
     })
     .catch((err) => {
         if (err) {
             throw err;
         }
     });
+}
+
+//Go through inquirer process with user to determine which subset of a table they want to view
+//Display the data
+async function ViewSubset(table,flag) {
+    if(table.name === 'employees') {
+        let questions = [];
+        if(flag === 'manager') {
+            questions.push({ name:'res', message:'Which manager\'s employees would you like to view?', type:'list', choices:GetInquirerOptionsFromTable(employees,['first_name','last_name']) });
+        } else if(flag==='role') {
+            questions.push({ name:'res', message:'Which role\'s staff would you like to view?', type:'list', choices:GetInquirerOptionsFromTable(roles,'title') });
+        } else {
+            return 'Invalid Flag Input';
+        }
+        inquirer.prompt([
+            ...questions
+        ])
+        .then(async answers => {
+            await ViewTable(table,flag + '_id',answers.res);
+            MainMenu();
+        })
+        .catch((err) => {
+            if (err) {
+                throw err;
+            }
+        });
+    } else {
+        console.log('Subset functionality not included for that table currently.');
+    }
 }
 
 //format a string with ' around it so that it can be used as a string elsewhere (such as DB queries)
@@ -251,8 +286,8 @@ async function GetInquirerOptionsFromTable(table,identifier) {
 }
 
 //displays table formatted in console
-async function ViewTable(table) {
-    const dat = await table.getData(db);
+async function ViewTable(table,field,value) {
+    const dat = await table.getData(db,field,value);
     if(dat.length) {
         console.log('\n');
         console.table(dat);
